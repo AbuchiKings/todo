c80ae547ff3b52314fdeb408210491fbb2e0cba7
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-08-06 13:36:20 +0100

    updated app.js

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const preventParameterPollution = require('hpp');
 const compression = require('compression');
 const cors = require('cors');
 const router = require('./routes/router');
+const http = require('http');
 
 const app = express();
 
@@ -21,14 +22,14 @@ mongoose.connect(process.env.MONGODB_URI, {
 }).then(con => console.log(`Connected to ${con.connections[0].name} Database successfully`))
     .catch(error => { return console.log(error); });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'ui')));
 
 app.use(cors());
 app.options('*', cors());
 
 app.use(helmet());
 app.use('/api', rateLimiter({
-    max: 1000,
+    max: 200,
     windowMs: 1000 * 60 * 60,
     message: 'Too many requests from this IP. Try again in an hour.'
 }));
@@ -38,10 +39,36 @@ app.use(cookieParser());
 
 app.use(sanitizeNosqlQuery());
 app.use(preventCrossSiteScripting());
-app.use(preventParameterPollution()); //Later on, some wanted duplicate fields can be whitelisted
+app.use(preventParameterPollution());
 
 app.use(compression());
 
+
 app.use(router);
 
-module.exports = app;
+
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 8080, () => {
+    console.log('Server is running on port 8080');
+});
+
+process.on('uncaughtException', (error) => {
+    console.log(error.name, error.message);
+    console.log(error);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (error) => {
+    console.log(error.name, error.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated!');
+    });
+});
